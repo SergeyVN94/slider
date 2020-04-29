806ccde6214c71bf68eea17a9414f2e8d376e8d9
SergeyVN94 <blakdgak1@yandex.ru>
2020-04-29 18:36:49 +0300

    Updated webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -4,66 +4,69 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const Webpack = require('webpack');
 
+const isProduction = process.env.NODE_ENV === 'production';
+const mode = isProduction ? 'production' : 'development';
+const devtool = isProduction ? 'nosources-source-map' : 'inline-source-map';
+
 module.exports = {
-    devServer: {
-        contentBase: `${__dirname}/dist`,
-        compress: true,
-        port: 9000,
-    },
-    context: `${__dirname}/src`,
-    entry: {
-        'index.js': './index.ts',
-    },
-    output: {
-        path: `${__dirname}/dist`,
-        filename: '[name]',
-    },
-    devtool: 'inline-source-map',
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.pug$/,
-                loader: 'pug-loader',
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.node$/,
-                use: 'node-loader',
-            },
+  mode,
+  devtool,
+  devServer: {
+    contentBase: `${__dirname}/dist`,
+    compress: true,
+    port: 9000,
+  },
+  context: `${__dirname}/src`,
+  entry: './index.ts',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
         ],
-    },
-    watchOptions: {
-        aggregateTimeout: 100,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.pug',
-            filename: 'index.html',
-        }),
-        new CopyPlugin([
-            {
-                from: 'chunks/favicons',
-                to: '',
-            },
-        ]),
-        new Webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-        }),
+      },
+      {
+        test: /\.node$/,
+        use: 'node-loader',
+      },
     ],
+  },
+  watchOptions: {
+    aggregateTimeout: 100,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.pug',
+      filename: 'index.html',
+    }),
+    new CopyPlugin([
+      {
+        from: 'chunks/favicons',
+        to: '',
+      },
+    ]),
+    new Webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
 };
