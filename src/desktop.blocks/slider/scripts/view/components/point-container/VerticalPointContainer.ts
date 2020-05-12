ef81e36a28bc660a31e63d686ff0ce4b696fa5d6
SergeyVN94 <blakdgak1@yandex.ru>
2020-05-12 15:17:53 +0300

    Fixed target position calculation algorithm.

--- src/desktop.blocks/slider/scripts/view/components/point-container/VerticalPointContainer.ts
+++ src/desktop.blocks/slider/scripts/view/components/point-container/VerticalPointContainer.ts
@@ -8,12 +8,12 @@ class VerticalPointContainer extends AbstractPointContainer {
   public getTargetPosition(ev: JQuery.MouseEventBase): number {
     const absolutePosition = ev.pageY;
     const offset = this.$pointContainer.offset().top;
-    const position = Math.abs(1 - ((absolutePosition - offset) / this.getSize()));
+    const position = (absolutePosition - offset) / this.getSize();
 
     if (position > 1) return 1;
     if (position < 0) return 0;
 
-    return position;
+    return 1 - position;
   }
 }
 
