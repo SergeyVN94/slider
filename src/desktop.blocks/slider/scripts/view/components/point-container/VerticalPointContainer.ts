82077458fe8bd2a459a95568f60f03af5829651f
SergeyVN94 <blakdgak1@yandex.ru>
2020-05-12 11:37:58 +0300

    Fixed bug when calculating the target position.

--- src/desktop.blocks/slider/scripts/view/components/point-container/VerticalPointContainer.ts
+++ src/desktop.blocks/slider/scripts/view/components/point-container/VerticalPointContainer.ts
@@ -8,7 +8,7 @@ class VerticalPointContainer extends AbstractPointContainer {
   public getTargetPosition(ev: JQuery.MouseEventBase): number {
     const absolutePosition = ev.pageY;
     const offset = this.$pointContainer.offset().top;
-    const position = (absolutePosition - offset) / this.getSize();
+    const position = Math.abs(1 - ((absolutePosition - offset) / this.getSize()));
 
     if (position > 1) return 1;
     if (position < 0) return 0;
